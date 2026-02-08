7f698a8bd0b0cbc8f61443c150f134209a76f7e6
Marco-online <11032488+Marco-online@users.noreply.github.com>
2026-02-08 09:25:02 +0100

    Changed Regex

--- Private/Get-UpdateLinks.ps1
+++ Private/Get-UpdateLinks.ps1
@@ -21,24 +21,37 @@ function Get-UpdateLinks {
     $DownloadDialog = Invoke-WebRequest @Params
     $Links = $DownloadDialog.Content -replace "www.download.windowsupdate", "download.windowsupdate"
 
-    $Regex = "downloadInformation\[0\]\.files\[\d+\]\.url\s*=\s*'([^']*kb(\d+)[^']*)'"
+    # NEW: Capture ALL downloadInformation arrays (not just [0])
+    # This regex matches downloadInformation[ANY_INDEX].files[ANY_INDEX].url
+    $Regex = "downloadInformation\[(\d+)\]\.files\[(\d+)\]\.url\s*=\s*'([^']*)'"
     $DownloadMatches = [regex]::Matches($Links, $Regex)
 
     if ($DownloadMatches.Count -eq 0) {
-        $RegexFallback = "downloadInformation\[0\]\.files\[0\]\.url\s*=\s*'([^']*)'"
-        $DownloadMatches = [regex]::Matches($Links, $RegexFallback)
-    }
-
-    if ($DownloadMatches.Count -eq 0) {
+        Write-Verbose "No download links found in response."
         return $null
     }
     
+    Write-Verbose "Found $($DownloadMatches.Count) download link(s) in response."
+    
     $KbLinks = foreach ($Match in $DownloadMatches) {
+        $Url = $Match.Groups[3].Value  # URL is in Group 3 now
+        
+        # Try to extract KB number from the URL (if present)
+        $KbNumber = 0
+        if ($Url -match 'kb(\d+)') {
+            $KbNumber = [int]$Matches[1]
+        }
+        
         [PSCustomObject]@{
-            URL = $Match.Groups[1].Value
-            KB  = if ($Match.Groups.Count -gt 2 -and $Match.Groups[2].Success) { [int]$Match.Groups[2].Value } else { 0 }
+            URL = $Url
+            KB  = $KbNumber
+            DownloadInfoIndex = [int]$Match.Groups[1].Value
+            FileIndex = [int]$Match.Groups[2].Value
         }
     }
     
-    return $KbLinks | Sort-Object KB -Descending
+    # Remove duplicates based on URL
+    $UniqueLinks = $KbLinks | Group-Object -Property URL | ForEach-Object { $_.Group[0] }
+    
+    return $UniqueLinks | Sort-Object KB -Descending
 }
